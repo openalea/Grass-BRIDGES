af78fdd81e2f3e081f2bd9b1e3dccde2b7f8cbf5
Tristan Gérault <tristan.gerault@uclouvain.be>
2026-08-11 18:08:40 +0200

    Shoot allocation to roots sensitive to nitrate export

--- simulations/hydro/analyze_simulation.py
+++ simulations/hydro/analyze_simulation.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     # output_path = os.path.join("outputs", "parametrization")
 
     # for scenario_name in ["WB_defense1_1_dbg_bal_1.1"]:
-    for scenario_name in ["WB_publish1_1_GB1"]:
+    for scenario_name in ["GB_CNW_1_250_vmaa_x50_low_SW_low_ph_kr"]:
     # for scenario_name in ["WB_def_soil_1_250_3d_2.1"]:
 
         # subscenarios = [subsc for subsc in os.listdir(os.path.join(output_path, scenario_name)) if subsc not in ["Soil", "Soil *", "Delete_to_Stop"]]
@@ -24,7 +24,7 @@ if __name__ == '__main__':
                                 inputs_dirpath="inputs",
                                 on_sums=True)
                 
-            if True:
+            if False:
                 # try:
                 analyze_data(scenarios=[scenario_name], outputs_dirpath=output_path, target_folder_key=subscenario,
                                 inputs_dirpath="inputs",
@@ -45,7 +45,7 @@ if __name__ == '__main__':
                                 on_mtg=True)
 
 
-            if False:
+            if True:
                 analyze_data(scenarios=[scenario_name], outputs_dirpath=output_path, target_folder_key="Soil",
                                         inputs_dirpath="inputs",
                                         on_sums=True,
--- simulations/hydro/simulation_cnw.py
+++ simulations/hydro/simulation_cnw.py
@@ -17,14 +17,14 @@ from openalea.fspm.utility.plot import analyze_data
 
 
 if __name__ == "__main__":
-    scenarios = ms.from_table(file_path="inputs/Scenarios_26-08-04.xlsx", which=["GB_CNW_1"])
-    custom_suffix = "vmaa_x10_vmNm_x5"
+    scenarios = ms.from_table(file_path="inputs/Scenarios_26-08-04.xlsx", which=["GB_1.0"])
+    custom_suffix = "vmNm_x100"
     output_folder = "/home/torisuten/Documents/outputs/wbr_outputs/test"
     time_step_in_seconds = 3600
     simulation_length_in_days = 20
     # n_iterations = int((simulation_length_in_days * 24 * 3600) / time_step_in_seconds) + 1
     n_iterations = 2500
-    densities = [1]
+    densities = [250]
     
     scene_xrange = 0.15
     scene_yrange = 0.15
